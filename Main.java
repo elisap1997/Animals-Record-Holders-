6df6c89653cc25acee90f3182435a01f54999b78
Elisa <52206763+elisap1997@users.noreply.github.com>
2022-03-23 11:30:31 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+
+  // Enter your code below this line
+  System.out.println("");
+  System.out.print("Hello and welcome to Animals Record Holders!");
+  System.out.println("Let's learn some interesting facts about animals!");
+  System.out.println("");
+
+  String animals = "ostrich";
+  int maxSpeed = 70;
+  int holdSpeed = 50;
+
+  System.out.println("Did you know that the " + animals + " is the fastest bird?");
+  System.out.println("");
+  
+  System.out.print("The " + animals + " can run up to " + maxSpeed + " km per hour.");
+  System.out.println(" Additionally, the " + animals + " can maintain a speed of " + holdSpeed + " km per hour for up to half an hour!");
+  System.out.println("");
+
+  System.out.println("Furthermore, the " + animals + " is the only bird with two toes on each foot!");
+  System.out.println("");
+
+  System.out.println("Despite its speed, the " + animals + " cannot fly. It is too heavy.");
+
+
+
+
+
+
+
+
+
+  /////////// End of the tasks /////////////////
+  } // end of method
+} // end of class
---
+++ README.md
@@ -0,0 +1,3 @@
+Animals Record Holders is a project made to showcase interesting facts about various animals. 
+
+This project was made in conjunction with Replit's Intro to Java curriculum. In this project, I use everything I learned in the exercises for storing values into variables and then to display text and number values.
